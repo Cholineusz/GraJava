3299072588ca6fbc147d202b9e8f31618603ff0a
Damian Cholewa <damian.cholewa.choli@gmail.com>
2013-02-22 20:23:00 +0100

    New

--- src/GraJavaPack/GraMain.java
+++ src/GraJavaPack/GraMain.java
@@ -1,33 +1,39 @@
 package GraJavaPack;
 import org.newdawn.slick.BasicGame;
+
 import java.lang.System;
+
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class GraMain extends BasicGame {
+	
+	private TiledMap map;
 
-	public GraMain(String title) {
-		super(title);
+	public GraMain() {
+		super("GraJava");
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		// TODO Auto-generated method stub
-
+		map.render(50, 50);
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		// TODO Auto-generated method stub
-
+		map = new TiledMap("res/Mapa/Mapa.tmx");
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	/**
@@ -35,7 +41,14 @@ public class GraMain extends BasicGame {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Nowosc");
+		try {
+			AppGameContainer app = new AppGameContainer(new GraMain());
+			app.setDisplayMode(500, 400, false);
+			app.start();
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
